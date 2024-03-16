69148e6de3e307a0f3db539079e31457c2535fbb
Your Name <enzothomazi@hotmail.com>
2024-03-16 11:25:36 -0300

    feat: Implementing entity

--- src/Graduation.Hackaton.VideoProcessing.Domain/Entities/VideoEntity.cs
+++ src/Graduation.Hackaton.VideoProcessing.Domain/Entities/VideoEntity.cs
@@ -1,7 +1,14 @@
-﻿namespace Graduation.Hackaton.VideoProcessing.Domain.Entities
+﻿using MongoDB.Bson;
+
+namespace Graduation.Hackaton.VideoProcessing.Domain.Entities
 {
     public class VideoEntity
     {
-
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
